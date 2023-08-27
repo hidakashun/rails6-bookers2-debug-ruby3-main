9fb703bb4139f4a2c9a0c27b048799a7db954dd9
EC2 Default User <ec2-user@ip-172-31-3-46.ap-southeast-2.compute.internal>
2023-08-27 09:49:23 +0000

    [update]応用課題２の２３

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
   has_many :books, dependent: :destroy#たくさんのbookを持っている1:N の関係性
-
-
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
-
-
+  validates :introduction, length: { maximum: 50 }
   has_one_attached :profile_image
   def get_profile_image(width, height)
     unless profile_image.attached?
