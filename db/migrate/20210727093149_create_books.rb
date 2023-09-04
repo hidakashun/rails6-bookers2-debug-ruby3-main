3a24bbf40f3b7be41f3ae52b086ba43123a6a22f
EC2 Default User <ec2-user@ip-172-31-3-46.ap-southeast-2.compute.internal>
2023-09-04 10:50:52 +0000

    [update]レイアウト

--- db/migrate/20210727093149_create_books.rb
+++ db/migrate/20210727093149_create_books.rb
@@ -4,6 +4,7 @@ class CreateBooks < ActiveRecord::Migration[6.1]
       t.string :title
       t.text :body
       t.integer :user_id
+      #t.float :rate0.5の評価を行うために必要だと思ったがわからず断念
       t.timestamps
     end
   end
